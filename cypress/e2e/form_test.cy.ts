7a55286ab7e1a7b0590471fe213ae5f5096e9f38
Diego Toledano <dtoledanob95@gmail.com>
2022-08-06 21:28:12 -0500

    command forms and tables, new index file

---
+++ cypress/e2e/form_test.cy.ts
@@ -0,0 +1,23 @@
+describe('subscription form test',() => {
+    beforeEach('visits local host', () =>{
+        cy.visit('http://localhost:3000')
+    })
+
+    it('successful subscription to form with correct email', () =>{
+        cy.getByData('email-input').type('diego@subform.com')
+        cy.getByData('submit-button').click()
+        cy.getByData('success-message').should('exist')
+    })
+
+    it('error message from subscription with bad email', () =>{
+        cy.getByData('email-input').type('diego')
+        cy.getByData('submit-button').click()
+        cy.getByData('success-message').should('not.exist')
+    })
+
+    it.only('error message already subscribed email', () =>{
+        cy.getByData('email-input').type('john@example.com')
+        cy.getByData('submit-button').click()
+        cy.getByData('server-error-message').should('exist')
+    })
+})
--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,14 @@
 describe('home page', () => {
-  it('visits local host', () => {
+
+  beforeEach('visits localhost',()=>{
     cy.visit('http://localhost:3000')
+  })
+
+  it('getting by element with command clean up', () => {
+    cy.getByData("hero-heading").contains('Testing Next.js Applications with Cypress')
+  })
+
+  it('testing list of features on dt tag with eq()', () =>{
+    cy.get("dt").eq(2).contains('Free and Open Source')
   })
 })
